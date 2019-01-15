04ea1f5063bdfae66d90a4266e4a44cc0017ff26
bitsdanielsalinas <daniel.salinas@bitsamericas.com>
2019-01-15 15:22:32 -0500

    funcion header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,40 @@
 $(document).ready(function() {
-    var scroll = $(window).scrollTop();
-    $('.limit').text(scroll);
-    $(window).scroll(function(event) {
+
+    var width = $(window).width();
+    if (width > 767) {
+
         var scroll = $(window).scrollTop();
         $('.limit').text(scroll);
-        /*Cambia el color del div cuando es distinto a 0*/
-        if (scroll != 0) {
-            $('.logo-contact').css({
-                width: '100%',
-                paddingTop: '3%'
-            });
-        } else {
-            $('.logo-contact').css({
-                width: '50%'
-            });
-        }
-    });
+        $(window).scroll(function(event) {
+            var scroll = $(window).scrollTop();
+            $('.limit').text(scroll);
+            /*Cambia el color del div cuando es distinto a 0*/
+            if (scroll != 0) {
+                $('.logo-contact').css({
+                    width: '100%',
+
+                });
+                $('.contact').css({
+                    paddingTop: '3%',
+                    paddingLeft: '38%'
+                });
+
+                $('.log').css({
+                    width: '8%'
+                });
+            } else {
+                $('.logo-contact').css({
+                    width: '50%'
+                });
+                $('.contact').css({
+                    paddingTop: '8%',
+                    paddingLeft: '0%'
+                });
+
+                $('.log').css({
+                    width: '13%'
+                });
+            }
+        });
+    }
 });
